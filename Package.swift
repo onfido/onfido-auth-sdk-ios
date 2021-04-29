720972b32d5f047de166ecd05ae68992b455bb11
onfido-ios-release <58592142+onfido-ios-release@users.noreply.github.com>
2021-04-29 16:33:23 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,6 +17,11 @@ let package = Package(
             name: "OnfidoAuth",
             url: "https://github.com/onfido/onfido-auth-sdk-ios/releases/download/0.1.0/OnfidoAuth-v0.1.0.zip",
             checksum: "312df54a6fab5a2f85da6acdb7f91d667f808c4b682d8e64b0af86ceaa283294"
+        ),
+        .binaryTarget(
+            name: "FaceTecSDK",
+            url: "https://github.com/onfido/onfido-auth-sdk-ios/releases/download/0.1.0/FaceTecSDK.zip",
+            checksum: "4ab4061a3af8a58446e1d422b3d71bee0becfc95e1717d52711fac03b555ba88"
         )
     ]
 )
