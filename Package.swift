32f1c2b53b24942e07112db0b17c54a37089b168
onfido-ios-release <58592142+onfido-ios-release@users.noreply.github.com>
2021-04-29 15:54:19 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "OnfidoAuth",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "OnfidoAuth",
+            targets: ["OnfidoAuth"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "OnfidoAuth",
+            url: "https://github.com/onfido/onfido-auth-sdk-ios/releases/download/0.1.0/OnfidoAuth-v0.1.0.zip",
+            checksum: "312df54a6fab5a2f85da6acdb7f91d667f808c4b682d8e64b0af86ceaa283294"
+        )
+    ]
+)
